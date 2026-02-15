828a0d258f027cc16f75cf19f0407a3a5f70ba6b
arno agten <arno.agten@gmail.com>
2026-02-14 21:46:41 -0500

    V1.0.7 - Pool mining incorporated, RPC edits, and other changes

--- launcher/start-mainnet.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting EquiForge Mainnet Miner...
-equiforge.exe node --mine --connect 129.80.239.237:9333
-pause
--- launcher/start-testnet.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting EquiForge Testnet Miner...
-equiforge.exe --testnet node --mine --connect 129.80.239.237:19333
-pause
